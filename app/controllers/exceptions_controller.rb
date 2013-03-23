19723926f9d5099c166ccbe75e90f91b49f0a23d
dilip <dilip@grepruby.com>
2013-03-23 15:26:02 +0530

    full error

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -3,18 +3,26 @@ class ExceptionsController < ApplicationController
   def exception
     project = Project.find_by_key(request.headers["X-API-Key"])
     application_error = params["error"]["backtrace"].select{|per| per["file"].include?("PROJECT_ROOT")}
+    source_error = params["error"]["source"]
     unless project.nil?
-      error_trace  = project.error_trace.select{|error_trace| error_trace.application == application_error}.first
+      error_trace  = project.error_trace.select{|error_trace| error_trace.source_code == source_error}.first
       if error_trace.blank?
         error = project.project_errors.create(:count=> 1,
           :desc => params["error"]["message"],
           :status => :active,
-          :title => params["error"]["class"] + " in " + params["controller"] + " # " + params["action"] +" in " + params["server"]["environment_name"],
+          :title => params["error"]["class"] + " in " + params["request"]["component"] + " # " + params["request"]["action"] +" in " + params["server"]["environment_name"],
           :url => params["request"]["url"],
           :generated_at => Time.now
         )
         error.create_error_trace(:application => application_error,
-          :full => params["error"]["backtrace"])
+          :full => params["error"]["backtrace"],
+          :source_code => params["error"]["source"],
+          :url => params["request"]["url"],
+          :params => params["request"]["params"],
+          :action => params["request"]["action"],
+          :environment => params["request"]["cgi_data"],
+          :browser => params["request"]["cgi_data"]["HTTP_USER_AGENT"],
+          :remote_ip => params["request"]["cgi_data"]["REMOTE_ADDR"])
       else
         error = error_trace.error
         error.count = error.count + 1
--- app/models/error_trace.rb
+++ app/models/error_trace.rb
@@ -1,7 +1,13 @@
 class ErrorTrace < ActiveRecord::Base
   belongs_to :error
-  attr_accessible :application, :full
-  
+  attr_accessible :application, :full, :source_code, :url, :params, :action, :environment, :context, :cookies, :remote_ip, :browser
+
   serialize :application, Array
   serialize :full, Array
+  serialize :source_code, Hash
+  serialize :params, Hash
+  serialize :environment, Hash
+  serialize :context, Hash
+  serialize :cookies, Hash
+
 end
