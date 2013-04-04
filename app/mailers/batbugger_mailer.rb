0fc0b7ee3fa1d72597f8d118c1ab89401f6f64ae
Dipak <dipak@grepruby.com>
2013-04-04 18:30:45 +0530

    style the error msg

--- app/mailers/batbugger_mailer.rb
+++ app/mailers/batbugger_mailer.rb
@@ -9,6 +9,6 @@ class BatbuggerMailer < ActionMailer::Base
   def exception(user_project, error)
     @error = error
     @project = user_project.project
-    mail to: user_project.user.email, subject: "Exception Notification : #{@project.name}"
+    mail to: user_project.user.email, subject: "BatBugger (#{@project.name} : #{error.title})"
   end
 end
