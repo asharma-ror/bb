fbdfed0a1faea8f471f2f6af54918b868cce02bc
Anil singh <anil.singh826@gmail.com>
2013-09-30 12:04:58 +0530

    chnaged the text og tracker and fixed the issue of changesset

--- app/controllers/changesets_controller.rb
+++ app/controllers/changesets_controller.rb
@@ -6,8 +6,8 @@ class ChangesetsController < ApplicationController
     @project = current_user.projects.find(params[:project_id])
     # FIXME extract method to model
     scope = @project.changesets.scoped
-    scope = scope.where('id <= ?', params[:to]) if params[:to]
-    scope = scope.where('id > ?', params[:from]) if params[:from]
+    scope = scope.where('id <= ?', params[:to]) unless params[:to].blank?
+    scope = scope.where('id > ?', params[:from]) unless params[:from].blank?
     @changesets = scope
     render :json => @changesets
   end
